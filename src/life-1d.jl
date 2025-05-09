c73d2254c46370fbe73a048042a00f84c4695dec
Peter Dresslar <peterdresslar@users.noreply.github.com>
2025-05-08 22:20:07 -1000

    stub out paste

--- src/life-1d.jl
+++ src/life-1d.jl
@@ -3,13 +3,17 @@ get_offsets(r::Int)::NTuple{8, Int} = (-r - 1, -r, -r + 1, -1, 1, r - 1, r, r + 
 
 init_board(N::Int, ρ::Float64)::BitVector = rand(N) .< ρ             # a function that initializes a board of size N with a given density ρ
 
+# paste_left_right(definition::Vector(Tuple(Int, Int)), origin::Int, r::Int, board::BitVector)::BitVector
+# this function will add in a primitive (spaceship or other) to the board at the origin
+# using the definitions in the tuple as Offset, r_multiplier.
+# the vector has a list of all the points that need to be set to alive to "paste" in the pattern.
 
 # ───────────── intra-step operations ─────────────
 
 @inline live_neighbors(b::BitVector, s::Int, offsets::NTuple{8, Int})::UInt8 =     # an inline function that counts the number of live neighbors of a cell
     count(offset -> begin                   # iterate over the eight offsets with a lambda function
             x = s + offset                  # absolute index of the neighbour, x, a point in neighborhood τᵣ
-            1 ≤ x ≤ length(b) && b[x]       # inside the board *and* alive?
+            1 ≤ x ≤ length(b) && b[x]       # inside the board *and* alive? NOTE off-board cells are all calculated as dead here.
         end,                                # lambda function ends here
         offsets)                            # neighborhood "addresses"
 
@@ -23,4 +27,3 @@ function step!(b::BitVector, tmp::BitVector, offsets::NTuple{8, Int})::BitVector
     copyto!(b, tmp)            # overwrite in-place
     return b
 end
-
